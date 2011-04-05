67a988077cdc52ed76d34c22059cf99df6a2b15a
Alex Garel <alex.garel@gmail.com>
2011-04-05 11:00:03 +0200

    documentation of code

--- src/dolmen/tales/slot.py
+++ src/dolmen/tales/slot.py
@@ -9,6 +9,8 @@ from chameleon.astutil import Symbol
 
 
 def query_slot(econtext, name):
+    """Compute the result of a slot expression
+    """
     context = econtext['context']
     request = econtext['request']
     view = econtext['view']
@@ -19,16 +21,16 @@ def query_slot(econtext, name):
 
 class SlotExpr(object):
     """
+    This is the interpreter of a slot: expression
     """
     def __init__(self, expression):
         self.expression = expression
-    
+
     def __call__(self, target, engine):
-        string = self.expression.strip()
+        slot_name = self.expression.strip()
         value = template(
             "query_slot(econtext, name)",
-            query_slot=Symbol(query_slot),
-            name=ast.Str(s=string),
-            mode="eval"
-            )
+            query_slot=Symbol(query_slot),  # ast of query_slot
+            name=ast.Str(s=slot_name),  # our name parameter to query_slot
+            mode="eval")
         return [ast.Assign(targets=[target], value=value)]
